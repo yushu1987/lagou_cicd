26c91f3e93e7c2bcceda187b6c3e23bf0d8cc08c
wangjian.king <wangjian.king@bytedance.com>
2022-11-27 10:24:34 +0800

    fix bug 4

--- shell/build.sh
+++ shell/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export GOROOT=/usr/local/opt/go/libexec
+#export GOROOT=/usr/local/opt/go/libexec
+export GOROOT=/usr/local/Cellar/go/1.17.2
+export GO111MODULE=on
+export GOPATH=/Users/wangjian/Code/GoProject
 if [ ! -d output ]; then
   mkdir output
 fi
