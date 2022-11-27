f724e21a16fbceac55dc961947c8642e1a634d86
wangjian.king <wangjian.king@bytedance.com>
2022-11-27 10:19:44 +0800

    fix bug 2

--- shell/build.sh
+++ shell/build.sh
@@ -3,10 +3,12 @@ export GOROOT=/usr/local/opt/go/libexec
 if [ ! -d output ]; then
   mkdir output
 fi
+rm -rf output/cicd
 go build -o  output/cicd cmd/echo/main.go
 #echo "build bin file"
 if [ -f output/cicd ];then
   echo "build successful"
+  exit 0
 else
   echo "build failed"
   exit -1
