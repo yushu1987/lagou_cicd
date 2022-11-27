d993a546ae8de273982a8532d2e25beb85886206
wangjian.king <wangjian.king@bytedance.com>
2022-11-27 10:27:00 +0800

    fix bug 6

--- shell/unit_test.sh
+++ shell/unit_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#export GOROOT=/usr/local/opt/go/libexec
+export GOROOT=/usr/local/opt/go/libexec
+#export GOROOT=/usr/local/Cellar/go/1.17.2
+export GO111MODULE=on
+export GOPATH=/Users/wangjian/Code/GoProject
 echo "run unit test"
 cd test
-go test .
+/usr/local/bin/go test .
